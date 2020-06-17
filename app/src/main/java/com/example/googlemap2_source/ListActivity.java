ecbce344fddf794e142b39bc143a5ed4a6f5b9b0
jinkoooooo <52557230+jinkoooooo@users.noreply.github.com>
2020-06-17 18:37:03 +0900

    초기커밋

---
+++ app/src/main/java/com/example/googlemap2_source/ListActivity.java
@@ -0,0 +1,59 @@
+package com.example.googlemap2_source;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+
+    public static List params;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        LinearLayout mainLayout = new LinearLayout(this);
+
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        for(int i=0 ;i<MapsActivity.SearchList.size();i++){
+
+            Button button = new Button(this);
+
+            button.setText(MapsActivity.SearchList.get(i).toString());
+
+            button.setLayoutParams(params);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent2 = new Intent(getApplicationContext(),ListClickActivity.class);
+                    startActivity(intent2);
+                }
+            });
+
+            mainLayout.addView(button);
+        }
+
+        setContentView(mainLayout);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/googlemap2_source/ListClickActivity.java
@@ -0,0 +1,22 @@
+package com.example.googlemap2_source;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class ListClickActivity extends AppCompatActivity {
+
+    public static List params;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+
+}
